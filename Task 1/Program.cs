70da9f49faaeb74f1d7fe439bf7e1b689f69f9f7
Roshna-karki <xettrirsohna12@gmail.com>
2025-12-03 23:58:56 +0545

    first commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Rectangle
+{
+    // Auto-properties
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    // Expression-bodied read-only properties
+    public double Area => Length * Width;
+    public double Perimeter => 2 * (Length + Width);
+
+    // Expression-bodied constructor
+    public Rectangle(double length, double width) =>
+        (Length, Width) = (length, width);
+
+    // Expression-bodied methods
+    public void PrintArea() => Console.WriteLine($"Area: {Area}");
+    public void PrintPerimeter() => Console.WriteLine($"Perimeter: {Perimeter}");
+}
+
+class Program
+{
+    static void Main()
+    {
+        var rect = new Rectangle(5, 3);
+        rect.PrintArea();
+        rect.PrintPerimeter();
+    }
+}
